94844aa60548c98d76b9b966231be45cf85208bf
Invictus-potens <felipeommelo@gmail.com>
2025-07-17 10:36:39 -0300

    33333

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -5,12 +5,10 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent = () => {
-  const [date, setDate] = useState<Date | [Date, Date] | null>(new Date());
+  const [date, setDate] = useState<Date | [Date | null, Date | null] | null>(new Date());
 
-  const handleChange = (value: Date | [Date, Date] | null) => {
-    if (value instanceof Date || (Array.isArray(value) && value.every(d => d instanceof Date))) {
-      setDate(value);
-    }
+  const handleChange = (value: Date | [Date | null, Date | null] | null) => {
+    setDate(value);
   };
 
   return (
