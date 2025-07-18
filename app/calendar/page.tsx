2a96679ef8c8d5eed82f0af577e3e15605a1ffe5
Invictus-potens <felipeommelo@gmail.com>
2025-07-18 12:24:42 -0300

    Refactor app build process to enhance performance and update dependency versions

---
+++ app/calendar/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import CalendarPage from "../../components/Calendar/calendar";
+
+export default function CalendarRoute() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Calendar</h1>
+      <CalendarPage />
+    </div>
+  );
+} 
